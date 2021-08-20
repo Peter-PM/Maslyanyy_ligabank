0a1c7e42bad67e03a254a31ced2562621748abfb
Peter-PM <tangompp@gmail.com>
2021-08-20 22:57:42 +0300

    верстка и стили

--- src/components/converter/converter.jsx
+++ src/components/converter/converter.jsx
@@ -13,7 +13,7 @@ function Converter() {
             <input className={styles.input} type="text" name="my-money-input" id="my-money-input" placeholder="1000"/>
           </label>
           <select className={styles.select} name="my-money" id="my-money">
-            <option value="rub" selected>RUB</option>
+            <option value="rub" defaultValue>RUB</option>
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
             <option value="gbr">GBR</option>
@@ -26,15 +26,15 @@ function Converter() {
             <input className={styles.input} type="text" name="buy-money-input" id="buy-money-input" placeholder="13,1234"/>
           </label>
           <select className={styles.select} name="buy-money" id="buy-money">
-            <option value="rub" selected>RUB</option>
+            <option value="rub" defaultValue>RUB</option>
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
             <option value="gbr">GBR</option>
             <option value="cny">CNY</option>
           </select>
         </fieldset>
-        <input className={`${styles.input} ${styles.inputDate}`} type="date" value="1.12.2020"/>
-        <button className={styles.button}>Сохранить результат</button>
+        <input className={`${styles.input} ${styles.inputDate}`} type="date"/>
+        <button className={styles.button} type="button">Сохранить результат</button>
       </form>
       <History/>
     </section>
