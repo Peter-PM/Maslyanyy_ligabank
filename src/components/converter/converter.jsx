eb9631e3f07b21da66ec48c6f86dd776cb5c0154
Peter-PM <tangompp@gmail.com>
2021-08-22 20:47:26 +0300

    conversion logic

--- src/components/converter/converter.jsx
+++ src/components/converter/converter.jsx
@@ -1,59 +1,79 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import History from '../history/history';
 import styles from './converter.module.scss';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
+
+const BACKEND_URL = 'https://openexchangerates.org/api/historical/';
+const APP_ID = '.json?app_id=b9501ac25219483780bf55b8ee16128a';
+const RUB = 'RUB';
+const USD = 'USD';
+const LIMITATION = 7;
+
 function Converter() {
   const currentDate = new Date();
 
   const [date, setDate] = useState(currentDate);
   const [isVisible, setIsVisible] = useState(false);
-  const [convertHistory, setHistory] = useState(new Array(10));
+  //const [isDisabled, setDisabled] = useState(false);
+  const [currency, setСurrency] = useState('');
+
+  // const [convertHistory, setHistory] = useState(new Array(10));
   const [myMoney, setMyMoney] = useState({
-    oneCurrency: 'RUB',
-    value: 0,
+    oneCurrency: RUB,
+    value: '',
   });
   const [buyMoney, setBuyMoney] = useState({
-    twoCurrency: 'USD',
-    value: 0,
+    twoCurrency: USD,
+    value: '',
   });
+
+  useEffect(() => {
+    fetch(`${BACKEND_URL}${dayjs(date).format('YYYY-MM-DD')}${APP_ID}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setСurrency(data.rates);
+      })
+      .catch(() => {
+      });
+  }, [date]);
+
+  useEffect(() => {
+    setBuyMoney(()=> ({
+      ...buyMoney,
+      value: +(myMoney.value/currency[myMoney.oneCurrency]*currency[buyMoney.twoCurrency]).toFixed(4) ? +(myMoney.value/currency[myMoney.oneCurrency]*currency[buyMoney.twoCurrency]).toFixed(4) : '',
+    }));
+  }, [currency]);
 
   const changeDate = (evt) => {
     setIsVisible(false);
     setDate(evt);
   };
 
-
-  const oneInput = document.querySelector('#my-money-input') ? document.querySelector('#my-money-input') : false;
-  const twoInput = document.querySelector('#buy-money-input') ? document.querySelector('#buy-money-input') : false;
-
-  const changeOneInput = (evt) => {
+  const changeOneInput = (value) => {
     setMyMoney(()=> ({
       ...myMoney,
-      value: +evt.target.value,
+      value: +value,
     }));
     setBuyMoney(()=> ({
       ...buyMoney,
-      value: +oneInput.value*2,
+      value: +(value/currency[myMoney.oneCurrency]*currency[buyMoney.twoCurrency]).toFixed(4),
     }));
   };
 
-  const changeTwoInput = (evt) => {
+  const changeTwoInput = (value) => {
     setBuyMoney(()=> ({
       ...buyMoney,
-      value: +evt.target.value,
+      value: +value,
     }));
 
     setMyMoney(()=> ({
       ...myMoney,
-      value: twoInput.value/2,
+      value: +(value*currency[myMoney.oneCurrency]/currency[buyMoney.twoCurrency]).toFixed(4),
     }));
-
   };
-
-  console.log(myMoney, buyMoney, oneInput, twoInput);
 
   return (
     <section className={styles.converter}>
@@ -70,7 +90,7 @@ function Converter() {
               placeholder="1000"
               value={myMoney.value}
               onChange={(evt) => {
-                changeOneInput(evt);
+                changeOneInput(evt.target.value);
               }}
             />
           </label>
@@ -78,18 +98,22 @@ function Converter() {
             className={styles.select}
             name="my-money"
             id="my-money"
-            defaultValue="RUB"
+            defaultValue={`${RUB}`}
             onChange={(evt) => {
               setMyMoney(()=> ({
                 ...myMoney,
                 oneCurrency: evt.target.value,
+              }));
+              setBuyMoney(()=> ({
+                ...buyMoney,
+                value: +(myMoney.value/currency[evt.target.value]*currency[buyMoney.twoCurrency]).toFixed(4),
               }));
             }}
           >
             <option value="RUB">RUB</option>
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
-            <option value="GBR">GBR</option>
+            <option value="GBP">GBP</option>
             <option value="CNY">CNY</option>
           </select>
         </fieldset>
@@ -104,7 +128,7 @@ function Converter() {
               placeholder="13,1234"
               value={buyMoney.value}
               onChange={(evt) => {
-                changeTwoInput(evt);
+                changeTwoInput(evt.target.value);
               }}
             />
           </label>
@@ -112,18 +136,18 @@ function Converter() {
             className={styles.select}
             name="buy-money"
             id="buy-money"
-            defaultValue="USD"
+            defaultValue={`${USD}`}
             onChange={(evt) => {
               setBuyMoney(()=> ({
-                ...buyMoney,
                 twoCurrency: evt.target.value,
+                value: +(myMoney.value/currency[myMoney.oneCurrency]*currency[evt.target.value]).toFixed(4),
               }));
             }}
           >
             <option value="RUB">RUB</option>
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
-            <option value="GBR">GBR</option>
+            <option value="GBP">GBP</option>
             <option value="CNY">CNY</option>
           </select>
         </fieldset>
@@ -139,11 +163,16 @@ function Converter() {
             className={styles.calendar}
             onChange={changeDate}
             value={date}
-            minDate={dayjs(currentDate).subtract(7, 'day').toDate()}
+            minDate={dayjs(currentDate).subtract(LIMITATION, 'day').toDate()}
             maxDate={currentDate}
           />
         )}
-        <button className={styles.button} type="button">Сохранить результат</button>
+        <button
+          className={styles.button}
+          type="button"
+        >
+          Сохранить результат
+        </button>
       </form>
       <History/>
     </section>
